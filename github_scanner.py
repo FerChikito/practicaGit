342b888ca60d23102e05b42361e3e586f236af5e
FerChikito <20243rd022@utez.edu.mx>
2025-09-17 16:02:15 -0600

    añadimos un bloque de codigo donde si hay repos los lista en pantalla numerados.

--- github_scanner.py
+++ github_scanner.py
@@ -10,3 +10,9 @@ git_dir = r"C:\Users\cfern\OneDrive\Desktop\practica git"
 repos = [name for name in os.listdir(git_dir)
          if os.path.isdir(os.path.join(git_dir, name)) and
          os.path.exists(os.path.join(git_dir, name, ".git"))]
+
+select_repo = None
+if repos:
+    print("found the following GitHub repository")
+    for idx, repo in enumerate(repos, 1):
+        print(f"{idx}. {repo}")
