0e955cc2f6e4e1657b1faccff76d220cb1f55eb5
FerChikito <20243rd022@utez.edu.mx>
2025-09-17 15:55:02 -0600

    Añadimos script python elistando el repositorio desde la carpeta asignada.

---
+++ github_scanner.py
@@ -0,0 +1,12 @@
+import os
+import subprocess
+import time
+import webbrowser
+#import requests
+import winreg
+
+git_dir = r"C:\Users\cfern\OneDrive\Desktop\practica git"
+
+repos = [name for name in os.listdir(git_dir)
+         if os.path.isdir(os.path.join(git_dir, name)) and
+         os.path.exists(os.path.join(git_dir, name, ".git"))]
